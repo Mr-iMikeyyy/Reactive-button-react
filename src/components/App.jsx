7e3ef274f9c803dc4344a24ec367897f5fb1565f
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-06-16 12:21:41 -0400

    Added last name functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,26 +2,46 @@ import React, { useState } from "react";
 
 function App() {
   const [mouseOver, setMouseOver] = useState(false);
-  const [name, setName] = useState("");
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const [fullName, setFullName] = useState("");
 
   function mouseLeaveSubmit() {
-    console.log("out");
     setMouseOver(false);
   }
 
   function mouseOverSubmit() {
-    console.log("over");
     setMouseOver(true);
   }
 
   function handleClick() {
-    setName(document.getElementById("formInput").value);
+    setFullName(fName + " " + lName);
+    console.log(fullName);
+  }
+
+  function handleChangeFName(event) {
+    setFName(event.target.value);
+  }
+
+  function handleChangeLName(event) {
+    setLName(event.target.value);
   }
 
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
-      <input id="formInput" type="text" placeholder="What's your name?" />
+      <h1>Hello {fullName}</h1>
+      <input
+        onChange={handleChangeFName}
+        type="text"
+        placeholder="What's your first name?"
+        value={fName}
+      />
+      <input
+        onChange={handleChangeLName}
+        type="text"
+        placeholder="What's your last name?"
+        value={lName}
+      />
       <button
         onMouseOver={mouseOverSubmit}
         onMouseOut={mouseLeaveSubmit}
