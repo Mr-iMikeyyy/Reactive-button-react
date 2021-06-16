4028efd94cf9bff487a709f458efce179aed7ee9
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-06-16 11:44:51 -0400

    holy **** it works, forgot the "r" in background...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [mouseOver, setMouseOver] = useState(false);
+
+  function mouseLeaveSubmit() {
+    console.log("out");
+    setMouseOver(false);
+  }
+
+  function mouseOverSubmit() {
+    console.log("over");
+    setMouseOver(true);
+  }
+
   return (
     <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        onMouseOver={mouseOverSubmit}
+        onMouseOut={mouseLeaveSubmit}
+        style={{ backgroundColor: mouseOver ? "black" : "white" }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
