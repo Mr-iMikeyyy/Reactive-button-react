0bb986e42a17515dc8714b955ed12ac569828da4
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-06-16 12:00:08 -0400

    updates header to say hello to whoever signed in

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function App() {
   const [mouseOver, setMouseOver] = useState(false);
+  const [name, setName] = useState("");
 
   function mouseLeaveSubmit() {
     console.log("out");
@@ -13,14 +14,19 @@ function App() {
     setMouseOver(true);
   }
 
+  function handleClick() {
+    setName(document.getElementById("formInput").value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>Hello {name}</h1>
+      <input id="formInput" type="text" placeholder="What's your name?" />
       <button
         onMouseOver={mouseOverSubmit}
         onMouseOut={mouseLeaveSubmit}
         style={{ backgroundColor: mouseOver ? "black" : "white" }}
+        onClick={handleClick}
       >
         Submit
       </button>
